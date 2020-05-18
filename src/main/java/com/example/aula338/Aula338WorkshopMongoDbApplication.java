42b7adebd66906a397cabafb49fe5d1f815aefa1
lexpierini <alex_pierini@hotmail.com>
2020-05-18 02:15:22 -0400

    Premier commit - projet créé

---
+++ src/main/java/com/example/aula338/Aula338WorkshopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula338;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula338WorkshopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula338WorkshopMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aula338/Aula338WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula338;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula338WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
