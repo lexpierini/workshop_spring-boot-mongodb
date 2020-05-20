8c406cf71562ada6b8daa63fc6e0f9e591d5a55b
lexpierini <alex_pierini@hotmail.com>
2020-05-20 00:34:31 -0400

    Mise à jour utilisateur avec PUT.

--- src/main/java/com/example/aula338/services/UserService.java
+++ src/main/java/com/example/aula338/services/UserService.java
@@ -13,28 +13,41 @@ import com.example.aula338.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
-	@Autowired // Instance un objet automatiquement. Mécanisme d'injection automatique de dépendances du Springboot.
+
+	@Autowired // Instance un objet automatiquement. Mécanisme d'injection automatique de
+				// dépendances du Springboot.
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objet non trouvé."));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
