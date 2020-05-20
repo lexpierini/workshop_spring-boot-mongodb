3ef34976e516c447d62beb389cb73f114a979057
lexpierini <alex_pierini@hotmail.com>
2020-05-20 12:33:07 -0400

    Requête simple avec des méthodes de requête.

--- src/main/java/com/example/aula338/repository/PostRepository.java
+++ src/main/java/com/example/aula338/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.aula338.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.aula338.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> { // L'objet UserRepository pourra effectuer des opérations de base avec des objets USER (enregistrer, récupérer, supprimer, mettre à jour) grâce à MongoRepository.
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
