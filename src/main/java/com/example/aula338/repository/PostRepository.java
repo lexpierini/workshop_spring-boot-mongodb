3d71e725b00861df8791beee1dda44ca7a6f59c4
lexpierini <alex_pierini@hotmail.com>
2020-05-20 12:39:37 -0400

    Requête simple avec @Query.

--- src/main/java/com/example/aula338/repository/PostRepository.java
+++ src/main/java/com/example/aula338/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.example.aula338.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.aula338.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> { // L'objet UserRepository pourra effectuer des opérations de base avec des objets USER (enregistrer, récupérer, supprimer, mettre à jour) grâce à MongoRepository.
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searcheTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
