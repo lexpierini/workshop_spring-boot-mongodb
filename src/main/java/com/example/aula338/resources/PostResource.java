4610319fa19087f016aa570e3ed2bb35cc94c7e3
lexpierini <alex_pierini@hotmail.com>
2020-05-20 11:45:35 -0400

    Obtenir une publication par id.

---
+++ src/main/java/com/example/aula338/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.example.aula338.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.aula338.domain.Post;
+import com.example.aula338.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired // Instance un objet automatiquement. Mécanisme d'injection automatique de dépendances du Springboot.
+	private PostService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/example/aula338/services/PostService.java
@@ -0,0 +1,23 @@
+package com.example.aula338.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aula338.domain.Post;
+import com.example.aula338.repository.PostRepository;
+import com.example.aula338.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired // Instance un objet automatiquement. Mécanisme d'injection automatique de dépendances du Springboot.
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objet non trouvé."));
+	}
+
+}
